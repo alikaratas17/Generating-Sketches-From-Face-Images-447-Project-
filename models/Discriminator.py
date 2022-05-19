7e285343b6708e4ab0d30f250128ec2daba3d953
alikaratas17 <72664046+alikaratas17@users.noreply.github.com>
2022-05-19 14:24:03 +0300

    Update Discriminator.py

--- models/Discriminator.py
+++ models/Discriminator.py
@@ -1,1 +1,16 @@
 
+import torch.nn as nn
+import torch.nn.functional as F
+import torch
+import numpy as np
+
+
+class Global_Discriminator(nn.Module):
+    def __init__(self):
+        super(Global_Discriminator, self).__init__()
+        pass
+
+class Local_Discriminator(nn.Module):
+    def __init__(self):
+        super(Local_Discriminator, self).__init__()
+        pass
